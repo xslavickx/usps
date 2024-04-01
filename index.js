dda5527ac060fd6b9d60b3f23bc24fd6be614989
slava <slava@shipserver.com>
2024-04-01 01:29:45 -0400

    added file.txt parser and write carrier response to new file

--- index.js
+++ index.js
@@ -1,41 +1,23 @@
+import fs from 'fs'
 import express from 'express'
 import { scrapeLogic } from './scrapeLogic.js'
-import pg from 'pg'
-import { prod_server } from './env.js'
+
 const app = express()
 const PORT = process.env.PORT || 4000
 
-//DB Connection
-async function queryDB(sql, params) {
-	const connection = new pg.Client()
-	connection.on('error', (err => {throw err}))
-	await connection.connect()
-	const result = await connection.query(sql, params)
-	await connection.end()
-	return result
-}
+const filePath = './incoming/input.txt';
 
-//DB List
-for(let server_index = 7; server_index < 8; server_index ++){
-	if(server_index == 13) {
-		continue 
-	}
+let fileContent = fs.readFileSync(filePath, 'utf-8');
+let trackingno_list = fileContent.split('\n');
 
-    prod_server(server_index)
+app.get('/scrape', (req, res) => {
+    scrapeLogic(res,trackingno_list)
+})
 
-    let get_trackingno_list = "select distinct dtl.trackingno from fdxinvoice inv join fdxinvoicedetail dtl using(invoicenum) join fdxinvcharge chr using(seq) where credate between current_date - 30 and current_date - 14 and code != 'FRT' and carriercode = 'USP'"
-    let trackingno_list = await queryDB(get_trackingno_list)
-    console.log(trackingno_list);
+app.get('/', (req, res) => {
+    res.send('Render Puppeteer server is up and running!')
+}) 
 
-    app.get('/scrape', (req, res) => {
-        scrapeLogic(res,trackingno_list)
-    })
-
-    app.get('/', (req, res) => {
-        res.send('Render Puppeteer server is up and running!')
-    }) 
-
-    app.listen(PORT, () => {
-        console.log(`Listening on port ${PORT}`)
-    })
-}
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`)
+})
--- scrapeLogic.js
+++ scrapeLogic.js
@@ -1,4 +1,6 @@
 import puppeteer from 'puppeteer'
+import {setTimeout} from "timers/promises"
+import fs from 'fs'
 import { config } from 'dotenv'
 
 export async function scrapeLogic (res,trackingno_list) {  
@@ -25,34 +27,55 @@ export async function scrapeLogic (res,trackingno_list) {
         let pages = await browser.newPage()
         let page = pages.length ? pages[0] : await browser.newPage()
         //throw new Error('whoops!')
+        
+        let page_login = false
+        do {
+            // Navigate the page to a URL
+            await Promise.all([ page.goto(login_page), page.waitForNavigation({ waitUntil:'networkidle2' }) ])
 
-        // Navigate the page to a URL
-        await Promise.all([ page.goto(login_page), page.waitForNavigation({ waitUntil:'networkidle2' }) ])
+            // Set screen size
+            await page.setViewport({width: 1080, height: 1024})
 
-        // Set screen size
-        await page.setViewport({width: 1080, height: 1024})
+            // Login
+            await page.waitForSelector('#username', { delay: 1000 }) 
+            await page.type('#username', 'ia_claims')
+            await page.type('#password', 'Welcome#12345', { delay: 500})
+            await Promise.all([ page.click('#btn-submit'), page.waitForNavigation({ waitUntil:'networkidle2' }) ])
 
-        // Login
-        await page.waitForSelector('#username', { delay: 1000 }) 
-        await page.type('#username', 'ia_claims')
-        await page.type('#password', 'Welcome#12345', { delay: 500})
-        await Promise.all([ page.click('#btn-submit'), page.waitForNavigation({ waitUntil:'networkidle2' }) ])
+            let welcome_msg = await page.evaluate(el => el.innerText, await page.$('.welcome.welcome-message'))
+            console.log(welcome_msg)
+            if(welcome_msg == 'Welcome, Slava Rubin') {
+                page_login = true
+                break
+            } else {
+                await setTimeout(60000)
+            }
+
+        } while(page_login == false)
 
         // Navigate to late claims submission page
         await Promise.all([ page.goto(late_domastic_claims), page.waitForNavigation({ waitUntil:'networkidle2' }) ])
 
-        for(let trackingno_counter = 0; trackingno_counter < trackingno_list.rows.length; trackingno_counter ++) {
+        for(let trackingno_counter = 0; trackingno_counter < trackingno_list.length; trackingno_counter ++) {
             // Sumbit claims
-            let tracking_num = trackingno_list.rows[trackingno_counter].trackingno
+            let tracking_num = trackingno_list[trackingno_counter]
             await page.waitForSelector('#tracking-numbers-control > div > div > ul > li > input', { delay: 1000 })
-            await page.type('#tracking-numbers-control > div > div > ul > li > input', tracking_num, { delay: 300})
-            await page.click('#tracking-numbers-control > div > div > ul > li > input', { delay: 50})
-            await page.click('.refunds-button-primary', { delay: 50})
+            await page.type('#tracking-numbers-control > div > div > ul > li > input', tracking_num.trim(), { delay: 300 })
+            await page.click('#tracking-numbers-control > div > div > ul > li > input', { delay: 100 })
+            await setTimeout(1500)
+            await page.click('.refunds-button-primary', { delay: 50 })
+            await setTimeout(3000)
 
             // Get response
             let claim_resolution = await page.evaluate(element => element.textContent, await page.$('.help-block.help-block--error span'))  
             console.log(claim_resolution)
+            await  appendText(claim_resolution)
             res.send(claim_resolution)
+            await page.click('#tracking-numbers-control > div > div > ul > li > input', { delay: 100 })
+            for (let i = 0; i < 4; i++) {
+                await page.keyboard.press('Backspace', { delay: 100})
+            }
+            await setTimeout(3000)
         }
 
     } catch (e){
@@ -63,4 +86,14 @@ export async function scrapeLogic (res,trackingno_list) {
        await browser.close()
     }
 
+}
+
+async function appendText(text) { 
+    fs.appendFile('./outgoing/output.txt', `${text}\r\n`, function(err) {
+      if(err) {
+          return console.log(err);
+      }
+    
+      console.log("The file was saved!");
+    });
 }
